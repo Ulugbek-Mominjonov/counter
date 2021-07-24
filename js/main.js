c10f45b065cffec61c8241e01e080c7779e7bb3e
Ulugbek-Mominjonov <ulugbeknabijonovich@gmail.com>
2021-07-24 09:33:02 +0500

    counter

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const btns = document.querySelectorAll('.btn'),
+      output = document.querySelector('.output');
+
+let count = 0;
+
+btns.forEach( btn => {
+  btn.addEventListener('click', () => {
+    const cssClasses = btn.classList;
+    if(cssClasses.contains('decrease')){
+      count--;
+    }else if(cssClasses.contains('inccrease')){
+      count++;
+    }else{
+      count = 0;
+    }
+
+    if(count < 0){
+      output.style.color = "red";
+    }else if(count > 0){
+      output.style.color = "green"
+    }else{
+      output.style.color = "#222"
+    }
+
+
+
+
+    output.textContent = count;
+  })
+})
